2c3a448677fa58c44a89e89365db2873e6fb87df
mkrukowski-catalogic <132896525+mkrukowski-catalogic@users.noreply.github.com>
2023-06-22 12:04:16 +0200

    Update generate_files.py

--- generate_files.py
+++ generate_files.py
@@ -2,11 +2,14 @@ import os
 from lorem_text import lorem
 from datetime import datetime
 
+# Number of files to be created
+x = 500
+
 current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 folder_name = "folder_{}".format(current_time)
 os.makedirs(folder_name, exist_ok=True)
 
-for i in range(1, 11):
+for i in range(1, x):
     file_name = "{}/file{}.txt".format(folder_name, i)
     with open(file_name, "w") as file:
         file.write(lorem.paragraph())
